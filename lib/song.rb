3197ca48e0a14b64f2b64dd11902596b8d09cd9b
Jason Crescenzo <crescenzojay@gmail.com>
2018-11-01 18:59:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,8 @@ class Song
   @@count = 0 
   @@genres = []
   @@artists = []
-  @@genre_count ={}
-  @@artist_count = {}
+  # @@genre_count ={}
+  # @@artist_count = {}
   
   def initialize(name, artist, genre)
     @name = name 
@@ -30,6 +30,7 @@ class Song
   end
   
   def self.genre_count 
+    @@genres.inject
   
 end 
 
